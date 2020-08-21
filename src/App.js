dc08a0299b2e2dcdc262a2ff67f348a16501dd5c
Linda <msh1730@hanmail.net>
2020-08-21 16:14:35 +0900

    로그인, 회원가입 view

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.css";
 import Mypage from "./Mypage";
+import SignIn from "./SignIn";
+import SignUp from "./SignUp";
 
 function App() {
   return (
     <div>
       <Mypage />
+      <SignIn />
+      <SignUp />
     </div>
   );
 }
